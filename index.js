60b8cec69b3da48c3672ebb6bd74bce0da888af0
+ <+>
2022-04-05 12:07:32 -0400

    commit tree e813f3a3a2561723d8a785fbae4abef15a9db2f0

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(param1) {
+    return cats.push(param1);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeFirstCat() {
+    let newArray = [...cats];
+    newArray.shift();
+    return newArray;
+}
